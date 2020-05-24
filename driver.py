82afb105312440f8ee45525b490466bef285d3b1
Sean Jordan <sjordan2@umbc.edu>
2020-05-23 21:34:40 -0400

    Added base module tests

---
+++ driver.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.insert(1, '/Users/seanathon48/PycharmProjects/Graph-Analytics/graph')
+from graph import Graph
+
+
+if __name__ == '__main__':
+    graph_test = Graph("Test Graph")
+    print(graph_test)
